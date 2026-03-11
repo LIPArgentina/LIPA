dd8f479b2c77ad97dd59e8c6a3cf2ce3c85ddba2
LIPA <ligaindependientedepool@gmail.com>
2026-03-11 17:16:20 -0300

    update backend url render

--- frontend/config.js
+++ frontend/config.js
@@ -2,5 +2,5 @@ window.APP_CONFIG = {
   API_BASE_URL:
     location.hostname === "localhost"
       ? "http://localhost:3000"
-      : "https://liga-backend.onrender.com"
+      : "https://liga-backend-tt82.onrender.com"
 };
